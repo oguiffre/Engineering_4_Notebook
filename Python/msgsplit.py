a0c76bf67bfa377d47014835dea82811bf95c19d
oguiffre <oguiffr94@charlottesvilleschools.org>
2018-09-12 11:33:10 -0400

    Adding my Strings and Loops Python program

---
+++ Python/msgsplit.py
@@ -0,0 +1,11 @@
+while True:
+        
+    msg = input("Write Your Message Here: ")
+
+    x = msg.split()
+
+        
+    for word in (x):
+        for letter in word:
+            print(letter)
+        print("-")
